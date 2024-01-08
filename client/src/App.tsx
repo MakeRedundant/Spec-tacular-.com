6b8721b1c8ae18367f0e6e36ac6fc88083da6272
MakeRedundant <105352286+MakeRedundant@users.noreply.github.com>
2024-01-09 00:50:32 +1100

    initial footer, header

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,18 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '../../public/vite.svg'
 import './App.css'
 
+import Landing from './pages/LandingPage/Landing.jsx';
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <Landing></Landing>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
