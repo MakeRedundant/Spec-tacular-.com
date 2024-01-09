e8669f41c3a7906c35910eec84bab17892e061de
MakeRedundant <105352286+MakeRedundant@users.noreply.github.com>
2024-01-09 20:57:54 +1100

    added prescription table

--- client/src/components/PrescriptionTable/index.tsx
+++ client/src/components/PrescriptionTable/index.tsx
@@ -5,81 +5,179 @@ import Text from "../Text";
 import * as S from "./styles";
 
 function PrescriptionTable() {
-    return (
-        <S.TableContainer>
-            <S.TableRow>
-                <Text text="PERTO" />
-            </S.TableRow>
-            <S.TableRow>
-                <S.TableHeader></S.TableHeader>
-                <S.TableHeader>ESF</S.TableHeader>
-                <S.TableHeader>CIL</S.TableHeader>
-                <S.TableHeader>EIXO</S.TableHeader>
-            </S.TableRow>
+  return (
+    <S.TableContainer>
+      <S.TableRow>
+        <Text text="Near" />
+      </S.TableRow>
+      <S.TableRow>
+        <S.TableHeader></S.TableHeader>
+        <S.TableHeader>SPH</S.TableHeader>
+        <S.TableHeader>CYL</S.TableHeader>
+        <S.TableHeader>AXIS</S.TableHeader>
+      </S.TableRow>
 
-            <S.TableRow>
-                <S.TableHeader>O.D.</S.TableHeader>
-                <S.TableData>
-                    <Input id="id" type="number" width="100%" height="20px" border="none" align />
-                </S.TableData>
-                <S.TableData>
-                    <Input id="id" type="number" width="100%" height="20px" border="none" align />
-                </S.TableData>
-                <S.TableData>
-                    <Input id="id" type="number" width="100%" height="20px" border="none" align />
-                </S.TableData>
-            </S.TableRow>
+      <S.TableRow>
+        <S.TableHeader>O.D||Right</S.TableHeader>
+        <S.TableData>
+          <Input
+            id="id"
+            type="number"
+            width="100%"
+            height="20px"
+            border="none"
+            align
+          />
+        </S.TableData>
+        <S.TableData>
+          <Input
+            id="id"
+            type="number"
+            width="100%"
+            height="20px"
+            border="none"
+            align
+          />
+        </S.TableData>
+        <S.TableData>
+          <Input
+            id="id"
+            type="number"
+            width="100%"
+            height="20px"
+            border="none"
+            align
+          />
+        </S.TableData>
+      </S.TableRow>
 
-            <S.TableRow>
-                <S.TableHeader>O.E.</S.TableHeader>
-                <S.TableData>
-                    <Input id="id" type="number" width="100%" height="20px" border="none" align />
-                </S.TableData>
-                <S.TableData>
-                    <Input id="id" type="number" width="100%" height="20px" border="none" align />
-                </S.TableData>
-                <S.TableData>
-                    <Input id="id" type="number" width="100%" height="20px" border="none" align />
-                </S.TableData>
-            </S.TableRow>
+      <S.TableRow>
+        <S.TableHeader>O.S||Left</S.TableHeader>
+        <S.TableData>
+          <Input
+            id="id"
+            type="number"
+            width="100%"
+            height="20px"
+            border="none"
+            align
+          />
+        </S.TableData>
+        <S.TableData>
+          <Input
+            id="id"
+            type="number"
+            width="100%"
+            height="20px"
+            border="none"
+            align
+          />
+        </S.TableData>
+        <S.TableData>
+          <Input
+            id="id"
+            type="number"
+            width="100%"
+            height="20px"
+            border="none"
+            align
+          />
+        </S.TableData>
+      </S.TableRow>
 
-            <S.TableRow>
-                <Text text="LONGE" />
-            </S.TableRow>
-            <S.TableRow>
-                <S.TableHeader></S.TableHeader>
-                <S.TableHeader>ESF</S.TableHeader>
-                <S.TableHeader>CIL</S.TableHeader>
-                <S.TableHeader>EIXO</S.TableHeader>
-            </S.TableRow>
+      <S.TableRow>
+        <Text text="Distance" />
+      </S.TableRow>
+      <S.TableRow>
+        <S.TableHeader></S.TableHeader>
+        <S.TableHeader>SPH</S.TableHeader>
+        <S.TableHeader>CYL</S.TableHeader>
+        <S.TableHeader>AXIS</S.TableHeader>
+      </S.TableRow>
 
-            <S.TableRow>
-                <S.TableHeader>O.D.</S.TableHeader>
-                <S.TableData>
-                    <Input id="id" type="number" width="100%" height="20px" border="none" align />
-                </S.TableData>
-                <S.TableData>
-                    <Input id="id" type="number" width="100%" height="20px" border="none" align />
-                </S.TableData>
-                <S.TableData>
-                    <Input id="id" type="number" width="100%" height="20px" border="none" align />
-                </S.TableData>
-            </S.TableRow>
+      <S.TableRow>
+        <S.TableHeader>O.D||Right</S.TableHeader>
+        <S.TableData>
+          <Input
+            id="id"
+            type="number"
+            width="100%"
+            height="20px"
+            border="none"
+            align
+          />
+        </S.TableData>
+        <S.TableData>
+          <Input
+            id="id"
+            type="number"
+            width="100%"
+            height="20px"
+            border="none"
+            align
+          />
+        </S.TableData>
+        <S.TableData>
+          <Input
+            id="id"
+            type="number"
+            width="100%"
+            height="20px"
+            border="none"
+            align
+          />
+        </S.TableData>
+      </S.TableRow>
 
-            <S.TableRow>
-                <S.TableHeader>O.E.</S.TableHeader>
-                <S.TableData>
-                    <Input id="id" type="number" width="100%" height="20px" border="none" align />
-                </S.TableData>
-                <S.TableData>
-                    <Input id="id" type="number" width="100%" height="20px" border="none" align />
-                </S.TableData>
-                <S.TableData>
-                    <Input id="id" type="number" width="100%" height="20px" border="none" align />
-                </S.TableData>
-            </S.TableRow>
-        </S.TableContainer>
-    );
+      <S.TableRow>
+        <S.TableHeader>O.S||Left</S.TableHeader>
+        <S.TableData>
+          <Input
+            id="id"
+            type="number"
+            width="100%"
+            height="20px"
+            border="none"
+            align
+          />
+        </S.TableData>
+        <S.TableData>
+          <Input
+            id="id"
+            type="number"
+            width="100%"
+            height="20px"
+            border="none"
+            align
+          />
+        </S.TableData>
+        <S.TableData>
+          <Input
+            id="id"
+            type="number"
+            width="100%"
+            height="20px"
+            border="none"
+            align
+          />
+        </S.TableData>
+      </S.TableRow>
+      {/* Add row placed below O.S||Left */}
+      <S.TableRow>
+        <S.TableHeader>Add</S.TableHeader>
+        <S.TableData>
+          <Input
+            id="id"
+            type="number"
+            width="100%"
+            height="20px"
+            border="none"
+            align
+          />
+        </S.TableData>
+      </S.TableRow>
+    </S.TableContainer>
+  );
 }
 
 export default PrescriptionTable;
