63d85fc92c037b5bccef918c4249863ca7799523
MakeRedundant <105352286+MakeRedundant@users.noreply.github.com>
2024-01-09 21:27:38 +1100

    added github, linked. resume links

--- client/src/components/Footer/index.tsx
+++ client/src/components/Footer/index.tsx
@@ -8,87 +8,66 @@ import { Link } from "react-router-dom";
 
 function Footer() {
     return (
-        <S.Footer>
-            <Container
-                flex
-                margin="auto"
-                width="calc(100% - 40px)"
-                direction="column"
-                justifyContent="center"
-                alignItems="center"
-            >
-                <Container
-                    flex
-                    margin="30px auto"
-                    width="100%"
-                    height="250px"
-                    direction="column"
-                    alignItems="center"
-                >
-                    <a
-                        target="_blank"
-                        href="https://www.google.com.br/maps/dir//Av.+Alberto+Carazzai,+1640+-+Centro,+Corn%C3%A9lio+Proc%C3%B3pio+-+PR,+86300-000/@-24.3255114,-50.6494976,15z"
-                        rel="noreferrer"
-                    >
-                        <Text
-                            text="Av. Alberto Carazzai, 1640 - Centro, Cornélio Procópio - PR, 86300-000"
-                            align="left"
-                            color={colors.primary}
-                            size="16px"
-                        />
-                    </a>
-
-                    <Text
-                        text="Loja virtual e consultoria para óculos e lentes de contato."
-                        align="left"
-                        color={colors.primary}
-                        size="18px"
-                    />
-
-                    <Container
-                        flex
-                        margin="50px auto"
-                        width="calc(100% - 500px)"
-                        justifyContent="space-evenly"
-                        alignItems="center"
-                    >
-                        <IconContainer
-                            className="fa fa-whatsapp"
-                            size={35}
-                            color={colors.primary}
-                        />
-
-                        <IconContainer
-                            className="fa fa-linkedin"
-                            size={35}
-                            color={colors.primary}
-                        />
-
-                        <IconContainer
-                            className="fa fa-facebook"
-                            size={35}
-                            color={colors.primary}
-                        />
-
-                        <IconContainer
-                            className="fa fa-instagram"
-                            size={35}
-                            color={colors.primary}
-                        />
-                    </Container>
-                    <Link to="/">
-                        <Text
-                            text="© 2024 Spec-tacular.com Made by Brian Trang"
-                            align="left"
-                            color={colors.primary}
-                            size="16px"
-                            weight={600}
-                        />
-                    </Link>
-                </Container>
-            </Container>
-        </S.Footer>
+      <S.Footer>
+        {/* Existing code */}
+        <Container
+          flex
+          margin="50px auto"
+          width="calc(100% - 500px)"
+          justifyContent="space-evenly"
+          alignItems="center"
+        >
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://github.com/MakeRedundant" // Github
+          >
+            <IconContainer
+              className="fa fa-github"
+              size={35}
+              color={colors.primary}
+            />
+          </a>
+  
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="www.linkedin.com/in/brian-trang-developer" // LinkedIn link
+          >
+            <IconContainer
+              className="fa fa-linkedin"
+              size={35}
+              color={colors.primary}
+            />
+          </a>
+  
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.facebook.com/your-facebook-profile" // Facebook link
+          >
+            <IconContainer
+              className="fa fa-facebook"
+              size={35}
+              color={colors.primary}
+            />
+          </a>
+  
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://drive.google.com/drive/folders/178aJT71OVJzvM4JJm0IqHat_YxyOVq7O"
+          >
+            <IconContainer
+              className="fa fa-file-pdf-o"
+              size={35}
+              color={colors.primary}
+            />
+          </a>
+        </Container>
+      </S.Footer>
     );
-}
-
-export default Footer;
+  }
+  
+  export default Footer;
+  
