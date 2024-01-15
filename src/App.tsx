f8b1d78735cce5b3d22e0ca0acc4dda15fe8d716
MakeRedundant <105352286+MakeRedundant@users.noreply.github.com>
2024-01-15 17:42:31 +1100

    fixing deployment error

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import Router from "./routes";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { reduxStore } from "./redux";
+import Welcome from "./pages/Welcome";
 
 function App() {
     return (
         <Provider store={reduxStore}>
             <BrowserRouter>
                 <Router />
+                <Welcome/>
             </BrowserRouter>
         </Provider>
     );
