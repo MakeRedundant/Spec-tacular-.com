aca04608fb0f915eccddcdb9ee7729a14c6b058d
MakeRedundant <105352286+MakeRedundant@users.noreply.github.com>
2024-01-15 17:18:01 +1100

    fixed typescript errors

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  build: {
-    chunkSizeWarningLimit: 100,
-    rollupOptions: {
-    onwarn(warning, warn) {
-      if (warning.code === 'MODULE_LEVEL_DIRECTIVE') {
-        return
-      }
-      warn(warning)
-    }}
-  },
   plugins: [react()],
   
 });
